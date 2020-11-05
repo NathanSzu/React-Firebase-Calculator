5e7e66a98a19cbc6c22d24de8979f56952c19c66
Nathan Szurek <nathan.szurek@yahoo.com>
2020-11-05 10:20:12 -0600

    Added HistoryItem component and map function to display past calculations

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './App.css';
 import Button from './components/Button'
+import HistoryItem from './components/HistoryItem'
 
 // Importing firebase SDK
 import firebase from 'firebase/app';
@@ -118,7 +119,7 @@ function App() {
                 {firstNum} {operator} {secondNum} {result}
               </div>
               <div className="col-4">
-                
+                {history && history.map(equation => <HistoryItem key={equation.id} text={equation.equation} date={equation.createdAt} />)}
               </div>
             </div>
           </div>
