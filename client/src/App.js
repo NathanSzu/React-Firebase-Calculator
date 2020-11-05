8e5de1879eeb14b8ec71a79879aa2381f64124cf
Nathan Szurek <nathan.szurek@yahoo.com>
2020-11-05 09:03:41 -0600

    Added imports for firebase and Bootstrap CDN link

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+
+// Importing firebase SDK
+import firebase from 'firebase/app';
+// Importing firestore database
+import 'firebase/firestore';
+// Importing firebase hooks to use in react
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAOdZQXcxWBZHU4azPDF4l95xByQlvWjUc",
+  authDomain: "calculator-861f4.firebaseapp.com",
+  databaseURL: "https://calculator-861f4.firebaseio.com",
+  projectId: "calculator-861f4",
+  storageBucket: "calculator-861f4.appspot.com",
+  messagingSenderId: "864317141433",
+  appId: "1:864317141433:web:4f3a00e6d2330e06588668"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Referencing firestore as a global variable
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div>
-      
+
     </div>
   );
 }
